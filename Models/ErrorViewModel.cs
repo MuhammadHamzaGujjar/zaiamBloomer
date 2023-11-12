58269b03b85385255f48f7dc70bc225bddcc71a3
Muhammad Hamza <66507164+MuhammadHamzaGujjar@users.noreply.github.com>
2023-11-12 14:25:02 +0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace zaiamBloomer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
